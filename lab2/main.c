dce178c5aeabcb9b35c5cdd6e964ec2ba71a4a37
faayeq <muhammadfaayeq@gmail.com>
2025-09-21 23:28:26 +0500

    build: lab2

--- lab2/main.c
+++ lab2/main.c
@@ -3,16 +3,23 @@
 #include <string.h>
 #define BUF 32
 
-int global_var = 42;                           // goes to .data
-static const char msg[] = "Hello from rodata"; // .rodata
+#ifdef DEBUG
+#define DEBUG_PRINTF(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+#define DEBUG_PRINTF(fmt, ...)
+#endif
+
+int global_var = 42;
+static const char msg[] = "Hello from rodata";
 
 int sum(int a, int b) { return a + b; }
 
 int main() {
-  char buf[BUF]; // buf in stack (.bss/stack)
+  char buf[BUF];
   snprintf(buf, sizeof(buf), "%s: %d\n", msg, global_var);
   puts(buf);
   int s = sum(10, 20);
+  DEBUG_PRINTF("Debug: sum is %d\n", s);
   printf("sum=%d\n", s);
   return 0;
 }
---
+++ lab2/main.s
@@ -0,0 +1,84 @@
+	.file	"main.c"
+	.text
+	.globl	global_var
+	.data
+	.align 4
+	.type	global_var, @object
+	.size	global_var, 4
+global_var:
+	.long	42
+	.section	.rodata
+	.align 16
+	.type	msg, @object
+	.size	msg, 18
+msg:
+	.string	"Hello from rodata"
+	.text
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum, .-sum
+	.section	.rodata
+.LC0:
+	.string	"%s: %d\n"
+.LC1:
+	.string	"sum=%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movl	global_var(%rip), %edx
+	leaq	-48(%rbp), %rax
+	movl	%edx, %r8d
+	movl	$msg, %ecx
+	movl	$.LC0, %edx
+	movl	$32, %esi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	snprintf
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rdi
+	call	puts
+	movl	$20, %esi
+	movl	$10, %edi
+	call	sum
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 15.2.1 20250808 (Red Hat 15.2.1-1)"
+	.section	.note.GNU-stack,"",@progbits
